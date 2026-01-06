f4cb12cfeec971283115a5cf1f320a70f820cb1c
Forai <159516259+sailornewborn@users.noreply.github.com>
2026-01-07 07:44:53 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const butt = document.getElementById("butt")
+const para = document.getElementById("para")
+
+butt.addEventListener("click", ()=>{
+  let totalSeconds = 10
+  const interval = setInterval(() => {
+    totalSeconds -= 1
+    para.textContent = `Time left: ${totalSeconds} seconds`
+    if (totalSeconds <= 0){
+      clearInterval(interval)
+      para.textContent = "Time left: TIME'S UPPPPPPP🔥"
+    }
+  }, 1000);
+})
